b91e71139499d25d96fdf3c498585753e56715e0
dano5342 <ellis.daniel0@gmail.com>
2020-09-30 10:19:54 +0200

    played with some of the styling settings for flex, alignment and positioning

--- App.js
+++ App.js
@@ -2,35 +2,42 @@ import React from "react";
 import {
   StyleSheet,
   View,
-  SafeAreaView,
-  Platform,
-  StatusBar,
-  Dimensions,
 } from "react-native";
 
-import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks'
 
 
 export default function App() {
-
-  const {landscape} = useDeviceOrientation();
-  console.log(useDeviceOrientation())
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={{
-        backgroundColor: 'dodgerblue',
-        width: '100%',
-        height: landscape ? "100%" : "30%"
-      }}>
-      </View>
-    </SafeAreaView>
+    <View style={{
+      backgroundColor: "#fff",
+      flex: 1,
+      flexDirection: 'row', // Horizontal
+      justifyContent: "center", // Main axis
+      alignItems: 'center', // secondary axis
+    }}>
+      <View 
+        style={{
+          backgroundColor: 'dodgerblue',
+          width: 100,
+          height: 300,
+          alignSelf: "flex-start",
+        }}
+      />
+      <View 
+        style={{
+          backgroundColor: 'gold',
+          width: 100,
+          height: 200,
+        }}
+      />
+      <View 
+        style={{
+          backgroundColor: 'tomato',
+          width: 100,
+          height: 100,
+        }}
+      />
+    </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-  },
-});
