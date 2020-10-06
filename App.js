0a45d8fbf6253726fb14143eeda9b161ece90c18
dano5342 <ellis.daniel0@gmail.com>
2020-10-06 11:54:32 +0200

    finished of styling to the buttons and new components, refactored code to go more in line with how app will look on company website

--- App.js
+++ App.js
@@ -1,21 +1,8 @@
 import React from "react";
-import {
-  StyleSheet,
-  View,
-  ImageBackground,
-  Text
-
-} from "react-native";
-
-import { MaterialCommunityIcons } from '@expo/vector-icons'
+import { View } from "react-native";
 
 import ViewImageScreen from "./app/screens/ViewImageScreen";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-import AppText from './app/components/AppText';
-import AppButton from './app/components/AppButton';
 
 export default function App() {
-  return (
-    <WelcomeScreen></WelcomeScreen>
-  );
+  return <ViewImageScreen />;
 }
--- app/components/AppText/index.js
+++
@@ -1,3 +0,0 @@
-import AppText from './AppText'
-
-export default AppText;
