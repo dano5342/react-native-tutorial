f7067e7277e919ce243fb977589dd34d1e5d90e0
dano5342 <ellis.daniel0@gmail.com>
2020-10-12 13:18:00 +0200

    starting to work on a TextInput reusable component

--- App.js
+++ App.js
@@ -1,13 +1,33 @@
-import React from "react";
-import { View } from "react-native";
+import React, {useState} from "react";
+import { TextInput, Text } from "react-native";
 
-
+import Screen from './app/components/Screen';
 import AccountScreen from "./app/screens/AccountScreen";
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 
 export default function App() {
+  // Needto useState for reactive elements in react
+  const [firstName, setFirstName] = useState('');
 
-  return <ListingDetailsScreen/>
+
+  return (
+    <Screen>
+      <Text>{firstName}</Text>
+      <TextInput
+        // maxLength
+        // keyboardType (good for setting numeric or other kb types)
+        // clearButtonMode={BOOL} // IOS only value, for clearing the value in the text box
+        // secureTextEntry={BOOL} = good for password handling
+        // clearButtonMode="always" -- IOS ONLY PROP
+        onChangeText={text => setFirstName(text)}
+        placeholder="First Name"
+        style={{
+          borderBottomColor: '#ccc',
+          borderBottomWidth: 1,
+        }}
+      />
+    </Screen>
+  )
 
 }
