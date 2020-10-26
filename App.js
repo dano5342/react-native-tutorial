0ac006e9eeacd7334b3f5c32a7d5fa5f850faa22
dano5342 <ellis.daniel0@gmail.com>
2020-10-26 14:50:57 +0100

    App.js contains a good overview of how a StackNavigator should work, also some elements should be visible that can be transferable skillwise to other types of app navigators

--- App.js
+++ App.js
@@ -1,10 +1,58 @@
 import React, { useState, useEffect } from "react";
-import ListingEditScreen from "./app/screens/ListingEditScreen";
+import { Button, Text } from 'react-native';
 
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
 
+import Screen from './app/components/Screen';
 
 export default function App() {
+
+  const Link = () => {
+    const navigation = useNavigation();
+    return (
+    <Button title="click" onPress={() => navigation.navigate("TweetDetails", {id: 1})}/>
+    )
+  }
+
+  const Tweets = ({ navigation }) => (
+    <Screen>
+      <Text> Tweets </Text>
+      <Link/>
+    </Screen>
+  )
+
+  const TweetDetails = ({ route }) => (
+    <Screen>
+      <Text>Tweet Details{route.params.id}</Text>
+    </Screen>
+  )
+  const Stack = createStackNavigator();
+  const StackNavigator = () => (
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: 'dodgerblue'},
+        headerTintColor: 'white',
+      }}
+    >
+      <Stack.Screen 
+        name="Tweets" 
+        component ={Tweets}
+        options={{  // Must be returned as an object or as some form of function
+          headerStyle: { backgroundColor: 'tomato'},
+          headerTintColor: 'white',
+        }}
+        />
+      <Stack.Screen 
+        name="TweetDetails"
+        component ={TweetDetails}
+        options={ ({ route }) => ({title: route.params.id})} // Programitically assign keys to properties
+        />
+    </Stack.Navigator>
+  )
   return (
-    <ListingEditScreen/>
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
   );
 }
