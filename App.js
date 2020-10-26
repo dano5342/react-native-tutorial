d80e64947246f110cafcd317ed02cb203153d7c3
dano5342 <ellis.daniel0@gmail.com>
2020-10-26 15:02:27 +0100

    added custom options for tab navigation. Working on nesting navigators next

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { Button, Text } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import {createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 import Screen from './app/components/Screen';
 
@@ -60,9 +61,28 @@ const Account = () => (
 
 const Tab = createBottomTabNavigator();
 const TabNavigator = () => (
-  <Tab.Navigator>
-    <Tab.Screen name="Feed" component={Tweets}/>
-    <Tab.Screen name="Account" component={Account}/>
+  <Tab.Navigator
+    tabBarOptions={{
+      activeBackgroundColor: "tomato",
+      activeTintColor: "white",
+      inactiveBackgroundColor: "#eee",
+      inactiveTintColor: "black"
+    }}
+  >
+    <Tab.Screen 
+      name="Feed" 
+      component={Tweets} 
+      options={{
+        tabBarIcon: ({size, color}) => <MaterialCommunityIcons name="home" size={size} color={color} />
+      }}
+    />
+    <Tab.Screen 
+      name="Account" 
+      component={Account} 
+      options={{
+        tabBarIcon: ({ size, color }) => <MaterialCommunityIcons name="account" size={size} color={color} />
+      }}
+      />
   </Tab.Navigator>
 )
 
