2df5afcba6ab94c7fad23bdc9d1faac5103ccb03
dano5342 <ellis.daniel0@gmail.com>
2020-10-26 11:39:52 +0100

    added image picker component to listingedit screen

--- App.js
+++ App.js
@@ -1,30 +1,10 @@
 import React, { useState, useEffect } from "react";
-import * as ImagePicker from 'expo-image-picker';
-import * as Permissions from 'expo-permissions';
+import ListingEditScreen from "./app/screens/ListingEditScreen";
 
 
-import Screen from './app/components/Screen';
-import ImageInput from "./app/components/ImageInput";
-import ImageInputList from "./app/components/ImageInputList";
 
 export default function App() {
-  const [imageUris, setImageUris] = useState([]);
-
-  const handleAdd = uri => {
-    setImageUris([...imageUris, uri]);
-  }
-  
-  const handleDelete = uri => {
-    setImageUris(imageUris.filter(imageUri => imageUri !== uri));
-  }
-
   return (
-    <Screen>
-      <ImageInputList
-        imageUris={imageUris}
-        onAddImage={handleAdd}
-        onRemoveImage={handleDelete}
-      />
-    </Screen>
+    <ListingEditScreen/>
   );
 }
--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -10,12 +10,14 @@ import {
 } from "../components/forms";
 import CategoryPickerItem from "../components/CategoryPickerItem";
 import Screen from "../components/Screen";
+import FormImagePicker from "../components/forms/FormImagePicker";
 
 const validationSchema = Yup.object().shape({
   title: Yup.string().required().min(1).label("Title"),
   price: Yup.number().required().min(1).max(10000).label("Price"),
   description: Yup.string().label("Description"),
   category: Yup.object().required().nullable().label("Category"),
+  images: Yup.array().min(1, "At least one image is required"),
 });
 
 const categories = [
@@ -84,10 +86,12 @@ function ListingEditScreen() {
           price: "",
           description: "",
           category: null,
+          images: [],
         }}
         onSubmit={(values) => console.log(values)}
         validationSchema={validationSchema}
       >
+        <FormImagePicker name="images" />
         <FormField maxLength={255} name="title" placeholder="Title" />
         <FormField
           keyboardType="numeric"
