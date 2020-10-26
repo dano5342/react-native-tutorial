f74af2e7a46cd2af6bdf065a02859bdd557aa1d0
dano5342 <ellis.daniel0@gmail.com>
2020-10-26 15:59:52 +0100

    implemented AuthNavigation, new files and namespace for navigation components

--- App.js
+++ App.js
@@ -1,96 +1,12 @@
+import { NavigationContainer } from "@react-navigation/native";
 import React, { useState, useEffect } from "react";
-import { Button, Text } from 'react-native';
-
-import { createStackNavigator } from '@react-navigation/stack';
-import {createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { NavigationContainer, useNavigation } from '@react-navigation/native';
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-
-import Screen from './app/components/Screen';
-
-
-const Link = () => {
-  const navigation = useNavigation();
-  return (
-  <Button title="click" onPress={() => navigation.navigate("TweetDetails", {id: 1})}/>
-  )
-}
-
-const Tweets = ({ navigation }) => (
-  <Screen>
-    <Text> Tweets </Text>
-    <Link/>
-  </Screen>
-)
-
-const TweetDetails = ({ route }) => (
-  <Screen>
-    <Text>Tweet Details{route.params.id}</Text>
-  </Screen>
-)
-
-const Stack = createStackNavigator();
-const StackNavigator = () => (
-  <Stack.Navigator
-    screenOptions={{
-      headerStyle: { backgroundColor: 'dodgerblue'},
-      headerTintColor: 'white',
-    }}
-  >
-    <Stack.Screen 
-      name="Tweets" 
-      component ={Tweets}
-      options={{  // Must be returned as an object or as some form of function
-        headerStyle: { backgroundColor: 'tomato'},
-        headerTintColor: 'white',
-      }}
-      />
-    <Stack.Screen 
-      name="TweetDetails"
-      component ={TweetDetails}
-      options={ ({ route }) => ({title: route.params.id})} // Programitically assign keys to properties
-      />
-  </Stack.Navigator>
-)
-
-const Account = () => (
-  <Screen>
-    <Text>Account</Text>
-  </Screen>
-)
-
-const Tab = createBottomTabNavigator();
-const TabNavigator = () => (
-  <Tab.Navigator
-    tabBarOptions={{
-      activeBackgroundColor: "tomato",
-      activeTintColor: "white",
-      inactiveBackgroundColor: "#eee",
-      inactiveTintColor: "black"
-    }}
-  >
-    <Tab.Screen 
-      name="Feed" 
-      component={Tweets} 
-      options={{
-        tabBarIcon: ({size, color}) => <MaterialCommunityIcons name="home" size={size} color={color} />
-      }}
-    />
-    <Tab.Screen 
-      name="Account" 
-      component={Account} 
-      options={{
-        tabBarIcon: ({ size, color }) => <MaterialCommunityIcons name="account" size={size} color={color} />
-      }}
-      />
-  </Tab.Navigator>
-)
+import AuthNavigator from "./app/navigation/AuthNavigator";
 
 export default function App() {
 
   return (
     <NavigationContainer>
-      <TabNavigator/>
+      <AuthNavigator/>
     </NavigationContainer>
   );
 }
--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { ImageBackground, StyleSheet, View, Image, Text } from "react-native";
+import AuthNavigation from '../navigation/AuthNavigator';
+
 
 import Button from "../components/Button";
 
-function WelcomeScreen(props) {
+function WelcomeScreen({ navigation }) {
   return (
     <ImageBackground
       blurRadius={10}
@@ -15,8 +17,8 @@ function WelcomeScreen(props) {
         <Text style={styles.tagline}>Sell What You Don't Need</Text>
       </View>
       <View style={styles.buttonsContainer}>
-        <Button title="Login" />
-        <Button title="Register" color="secondary" />
+        <Button title="Login" onPress={() => navigation.navigate('Login')} />
+        <Button title="Register" color="secondary" onPress={() => navigation.navigate('Register')} />
       </View>
     </ImageBackground>
   );
